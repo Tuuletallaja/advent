56dd7ed1b1f54d0c2e4b4dd8037c01da98bd38ed
Tuuletallaja <masugota@gmail.com>
2019-12-11 14:24:11 +0200

    finished part 1

--- day_1.php
+++ day_1.php
@@ -1,3 +1,17 @@
 <?php
 
 echo "hello\n";
+
+$input = file_get_contents("input.txt");
+
+//part 1
+
+$up = substr_count($input, '(');
+$down = substr_count($input, ')');
+
+echo $up . "\n";
+echo $down . "\n";
+
+echo ($up - $down);
+
+//part 2
